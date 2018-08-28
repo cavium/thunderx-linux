1d57b16fc90d99bb5bc89e677c5ef4ecadbf7fa8
Robert Richter <rrichter@cavium.com>
2018-08-28 11:09:48 +0200

    Revert "arm64: acpi: fix alignment fault in accessing ACPI"
    This reverts commit 471a489b66256adf01516e56a12a2d8247cb5151.
    
    Signed-off-by: Robert Richter <rrichter@cavium.com>

--- arch/arm64/kernel/acpi.c
+++ arch/arm64/kernel/acpi.c
@@ -18,7 +18,6 @@
 #include <linux/acpi.h>
 #include <linux/bootmem.h>
 #include <linux/cpumask.h>
-#include <linux/efi.h>
 #include <linux/efi-bgrt.h>
 #include <linux/init.h>
 #include <linux/irq.h>
@@ -30,8 +29,12 @@
 
 #include <asm/cputype.h>
 #include <asm/cpu_ops.h>
-#include <asm/pgtable.h>
 #include <asm/smp_plat.h>
+
+#ifdef CONFIG_ACPI_APEI
+# include <linux/efi.h>
+# include <asm/pgtable.h>
+#endif
 
 int acpi_noirq = 1;		/* skip ACPI IRQ initialization */
 int acpi_disabled = 1;
@@ -236,7 +239,8 @@ done:
 	}
 }
 
-pgprot_t __acpi_get_mem_attribute(phys_addr_t addr)
+#ifdef CONFIG_ACPI_APEI
+pgprot_t arch_apei_get_mem_attribute(phys_addr_t addr)
 {
 	/*
 	 * According to "Table 8 Map: EFI memory types to AArch64 memory
@@ -257,3 +261,4 @@ pgprot_t __acpi_get_mem_attribute(phys_addr_t addr)
 		return __pgprot(PROT_NORMAL_NC);
 	return __pgprot(PROT_DEVICE_nGnRnE);
 }
+#endif
